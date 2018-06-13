59db8081fa0f3b03c6432268b0ac84cec89748de
DebugScientist78 <40225678+DebugScientist78@users.noreply.github.com>
2018-06-13 01:56:19 -0400

    Add files via upload

---
+++ Keyword/keyword.js
@@ -0,0 +1,27 @@
+
+
+var input_string = "";
+var keywords = [];
+var total_words_per_input = 0;
+var words = [];
+var word_spell_out = "";
+var key_phrases = [];
+
+// Develop a algorithum in which 
+
+function set_input_string() {
+    input_string = document.getElementById("input_text").value;
+    document.getElementById("input_string").innerHTML = input_string;
+    words = input_string.split(" ");
+    total_words_per_input = words.length;
+    document.getElementById("word_array").innerHTML = words;
+    document.getElementById("num_of_words").innerHTML = words.length;
+
+    key_phrases = 
+    [
+        "How much calories should I have daily?",
+        "What is diabetes?",
+        "Why is it important to sleep?",
+    ];
+    
+}
